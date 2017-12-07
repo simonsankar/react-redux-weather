2727cd26ac224f3c9ff265b157b3ce10a08216a9
simonsankar <simonlfcsuarez7@gmail.com>
2017-12-07 12:35:12 -0400

    Minor changes
    Title and desc updated

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -4,6 +4,7 @@ const Jumbotron = () => (
   <div className="jumbotron">
       <h1>Quick Weather</h1>
       <p>How's the weather?</p>
+      <p className="text-muted">Data shown is a 3 day forecast.</p>
   </div>
 );
 
