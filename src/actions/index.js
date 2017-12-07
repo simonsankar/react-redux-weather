6e478e361d57250fba84caf2a114a3066ecaa673
simonsankar <simonlfcsuarez7@gmail.com>
2017-12-07 12:17:26 -0400

    API Link change

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';  // To make ajax requests
 import { FETCH_WEATHER } from '../types/types';
 
 const API_KEY = '2552a2f7906dd4cc64351fa8f24c59b9';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const fetchWeather = (city) => {
   const url = `${ROOT_URL}&q=${city},tt&units=metric&cnt=16`; // TT for Trinidad
